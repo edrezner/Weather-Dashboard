9fd1dabbb6fc473e745de90dc9baf5d9b7abe964
Seven <edrezner7@gmail.com>
2023-03-21 10:31:06 -0400

    Added top div to show current day weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@ var search = $(".search");
 var aside = $(".aside");
 var cityArray = JSON.parse(localStorage.getItem("City")) || [];
 for (i = 0; i < cityArray.length; i++) {
-    var cityBtn = $("<button class='my-2'>");
+    var cityBtn = $("<button class='m-2'>");
     cityBtn.text(cityArray[i]);
     aside.append(cityBtn);
 }
+
 
 search.click(function () {
     // Set input string into api call
@@ -25,13 +26,46 @@ search.click(function () {
 
     localStorage.setItem("City", JSON.stringify(recentSearches));
 
-    var apiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityString + "&appid=c0c7982eea1a651cf4a03ef899b1c02f"
+    // var zeroIndexBtn = $("<button class='m-2'>");
+    // zeroIndexBtn.text(cityArray[0]);
+    // aside.append(zeroIndexBtn);
+
+    var apiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityString + "&appid=c0c7982eea1a651cf4a03ef899b1c02f&units=imperial"
 
     fetch(apiCall)
         .then(response => response.json())
         .then(data => {
-            console.log(data); 
+            console.log(data);
+
+
+            // $("input").val("");
+
+            // create top portion of dashboard with city name, date, temp, wind, and humidity
+            var content = $(".content");
+            content.innerHTML = "";
+
+            var topDiv = $("<div>").addClass("border border-dark m-3");
+            content.append(topDiv);
+
+            var h2 = $("<h2>").addClass("m-2");
+            var cityH2 = data.city.name;
+            h2.text(cityH2);
+            topDiv.append(h2);
+
+            var pTemp = $("<p>").addClass("m-2");
+            var currentTemp = data.list[0].main.temp;
+            pTemp.text("Temp: " + currentTemp + "\u00B0F");
+            topDiv.append(pTemp);
+
+            var pWind = $("<p>").addClass("m-2");
+            var currentWind = data.list[0].wind.speed;
+            pWind.text("Wind: " + currentWind + " MPH");
+            topDiv.append(pWind);
+
+            var pHumid = $("<p>").addClass("m-2");
+            var currentHumid = data.list[0].main.humidity;
+            pHumid.text("Humidity: " + currentHumid + "%");
+            topDiv.append(pHumid)
         })
-    
-    $("input").val("");
 })
+
