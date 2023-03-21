0c9ce070866663f51e241f50ac5b684d976f8c4d
Seven <edrezner7@gmail.com>
2023-03-21 16:23:03 -0400

    Added bottom div with 5 day forecast information; some styling added

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ search.click(function () {
     // zeroIndexBtn.text(cityArray[0]);
     // aside.append(zeroIndexBtn);
 
-    var apiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityString + "&appid=c0c7982eea1a651cf4a03ef899b1c02f&units=imperial"
+    var apiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityString + "&exclude=hourly&appid=c0c7982eea1a651cf4a03ef899b1c02f&units=imperial"
 
     fetch(apiCall)
         .then(response => response.json())
@@ -40,7 +40,7 @@ search.click(function () {
 
             // $("input").val("");
 
-            // create top portion of dashboard with city name, date, temp, wind, and humidity
+            // create top portion of dashboard with city name, date, weather icon, temp, wind, and humidity
             var content = $(".content");
             content.innerHTML = "";
 
@@ -48,8 +48,11 @@ search.click(function () {
             content.append(topDiv);
 
             var h2 = $("<h2>").addClass("m-2");
-            var cityH2 = data.city.name;
+            var cityH2 = data.city.name + " " + dayjs(data.list[0].dt_txt).format("M/D/YYYY");
+            var weatherIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png")
+                     .attr("alt", "Weather Icon");
             h2.text(cityH2);
+            h2.append(weatherIcon);
             topDiv.append(h2);
 
             var pTemp = $("<p>").addClass("m-2");
@@ -66,6 +69,39 @@ search.click(function () {
             var currentHumid = data.list[0].main.humidity;
             pHumid.text("Humidity: " + currentHumid + "%");
             topDiv.append(pHumid)
+
+            // create bottom portion of dashboard with weather forecast info for next 5 days
+
+            var bottomDiv = $("<div>").addClass("d-flex flex-wrap justify-content-between");
+            content.append(bottomDiv);
+
+            var fcH3Div = $("<div>").addClass("col-12 mb-3");
+            bottomDiv.append(fcH3Div);
+            
+            var fcH3 = $("<h3>").addClass("m-3").text("5-Day Forecast:")
+            fcH3Div.append(fcH3);
+
+
+            for (i=0; i < data.list.length; i += 8) {
+                var fcCard = $("<div>").addClass("col-2 m-3 border border-dark fc");
+                bottomDiv.append(fcCard);
+
+                var fcDate = $("<h5>").addClass("mx-2 my-3").text(dayjs(data.list[i].dt_txt).format("M/D/YYYY"));
+                fcCard.append(fcDate);
+                
+                var fcIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png")
+                .attr("alt", "Weather Icon");
+                fcCard.append(fcIcon);
+
+                var fcTemp = $("<p>").addClass("mx-2 my-3").text("Temp: " + data.list[i].main.temp + "\u00B0F");
+                fcCard.append(fcTemp);
+
+                var fcWind = $("<p>").addClass("mx-2 my-3").text("Wind: " + data.list[i].wind.speed + " MPH");
+                fcCard.append(fcWind);
+
+                var fcHumid = $("<p>").addClass("mx-2 my-3").text("Humidity: " + data.list[i].main.humidity + "%");
+                fcCard.append(fcHumid);
+            }
         })
 })
 
